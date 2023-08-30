e9f53831978ebcebb1ccd2ee572cecaa5f67e41d
xijinping0 <111333464+xijinping0@users.noreply.github.com>
2023-08-30 14:12:29 -0700

    create repo

---
+++ README.md
@@ -0,0 +1,34 @@
+# [红色赌盘](https://roulette.xijinping.one/docs/toc)
+
+[![Deploy](https://github.com/xijinping0/roulette/actions/workflows/deploy.yaml/badge.svg)](https://github.com/xijinping0/roulette/actions/workflows/deploy.yaml)
+
+作者：沈栋
+
+> 寧鳴而死，不默而生
+>
+> Better to speak out and die than keep silent and live.
+>
+> ——范仲淹（989 - 1052）
+
+## 目录
+
+- [前言](https://roulette.xijinping.one/docs/ch00)
+- [第一章](https://roulette.xijinping.one/docs/ch01)
+
+## 开发
+
+本项目使用 [Docusaurus](https://docusaurus.io/docs) 开发。
+
+```sh
+yarn install
+yarn start
+```
+
+## 部署
+
+- 推送到 `master` 的改动，会自动发布到 Github Page。
+- 手动部署
+
+  ```sh
+  GIT_USER=<Your GitHub username> yarn deploy
+  ```
---
+++ docusaurus.config.js
@@ -0,0 +1,99 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: '红色赌盘',
+  tagline: '电子全本',
+  url: 'https://roulette.xijinping.one',
+  baseUrl: '/',
+  onBrokenLinks: 'ignore',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/cover.jpg',
+  trailingSlash: false,
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'xijinping0', // Usually your GitHub org/user name.
+  projectName: 'roulette', // Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'zh-Hans',
+    locales: ['zh-Hans'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          breadcrumbs: true,
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl: 'https://github.com/xijinping0/roulette/tree/master/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: '红色赌盘',
+        logo: {
+          href: '/docs/toc',
+          src: 'img/cover.jpg',
+        },
+        items: [
+          {
+            href: 'https://github.com/xijinping0/roulette',
+            label: '源代码',
+            position: 'right',
+          },
+        ],
+      },
+      docs: {
+        sidebar: {
+          hideable: true,
+        },
+      },
+      footer: {
+        style: 'dark',
+        copyright: `Copyright © 2021 沈栋`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+  scripts: [
+    {
+      src: 'https://plausible.io/js/script.js',
+      defer: true,
+      'data-domain': 'xijinping.one',
+    }],
+  themes: [
+    [
+      "@easyops-cn/docusaurus-search-local",
+      {
+        hashed: true,
+        language: ["en", "zh"],
+        highlightSearchTermsOnTargetPage: true,
+        explicitSearchResultPath: true,
+      },
+    ],
+  ],
+};
+
+module.exports = config;
