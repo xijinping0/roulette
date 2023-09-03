ca017e4e81663c57f389717acd57b8fe615ef4a9
xijinping0 <111333464+xijinping0@users.noreply.github.com>
2023-09-02 19:26:35 -0700

    add remaining chapters

--- sidebars.js
+++ sidebars.js
@@ -16,10 +16,28 @@ const sidebars = {
   // By default, Docusaurus generates a sidebar from the docs folder structure
   docs: [
     'toc',
+    'author',
     'ch00',
     'ch01',
     'ch02',
     'ch03',
+    'ch04',
+    'ch05',
+    'ch06',
+    'ch07',
+    'ch08',
+    'ch09',
+    'ch10',
+    'ch11',
+    'ch12',
+    'ch13',
+    'ch14',
+    'ch15',
+    'ch16',
+    'ch17',
+    'ch18',
+    'photos',
+    'acknowledgement',
   ],
 
   // But you can create a sidebar manually
